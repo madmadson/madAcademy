86d39f8a378802d3b3a69a916893bdf51bf8562e
Tobias Matt <tobias.matt@googlemail.com>
2023-03-04 22:17:45 +0100

    initial commit

---
+++ src/main/java/de/madAcademy/MadAcademyApplication.java
@@ -0,0 +1,13 @@
+package de.madAcademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadAcademyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/madAcademy/MadAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package de.madAcademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
